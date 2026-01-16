69816b6b4c05be84ac7b024e469712562adcf01f
jguese3 <jguese3@rrc.ca>
2026-01-16 11:01:24 -0600

    Feat: Implemented backlog component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css'
 import { Footer } from './components/footer/footer'
 import { FriendStatus } from './components/friend_status/friend_status'
 import GameReview from './components/GameReview/GameReview'
+import { Backlog } from './components/backlog/backlog'
 
 function App() {
   return (
     <>
+      <Backlog/>
       <GameReview/>
+      <br></br>
+      <h1>Friend Status</h1>
       <FriendStatus/>
       <Footer />
     </>
