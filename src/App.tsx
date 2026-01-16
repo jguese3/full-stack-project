a88abe23c0d5caa98fd1beb64e675c4e479b58a8
jguese3 <jguese3@rrc.ca>
2026-01-16 10:46:36 -0600

    Feat: Imported new components to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Footer } from './components/footer/footer'
 import { FriendStatus } from './components/friend_status/friend_status'
+import GameReview from './components/GameReview/GameReview'
 
 function App() {
   return (
     <>
+      <GameReview/>
       <FriendStatus/>
       <Footer />
     </>
