2aaa7be4e8074045bf555553d10b4d892c336bba
jguese3 <jguese3@rrc.ca>
2026-01-18 21:47:29 -0600

    Feat: Updated CSS to add styling for the header

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Footer } from './components/footer/footer'
 import { FriendStatus } from './components/friend_status/friend_status'
 import GameReview from './components/GameReview/GameReview'
 import { Backlog } from './components/backlog/backlog'
+import { Header } from './components/header/header'
 
 function App() {
   return (
     <>
+      <Header/>
       <Backlog/>
       <GameReview/>
       <FriendStatus/>
