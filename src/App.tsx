bf420e55b93036a25dc538ca9fdca862246271f9
Christian Martin <cmartin60@academic.rrc.ca>
2026-01-20 14:40:33 -0600

    Update: App.tsx to add the Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import './App.css'
+import { Header } from './components/header/header'
 import { Footer } from './components/footer/footer'
+
+
 import { FriendStatus } from './components/friend_status/friend_status'
 import GameReview from './components/GameReview/GameReview'
 import { Backlog } from './components/backlog/backlog'
-import { Header } from './components/header/header'
+
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
     <>
-      <Header/>
-      <Backlog/>
-      <GameReview/>
-      <FriendStatus/>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+        />
+        <Route path="/backlog" element={<Backlog />} />
+        <Route path="/reviews" element={<GameReview />} />
+        <Route path="/friends" element={<FriendStatus />} />
+      </Routes>
       <Footer />
     </>
   )
