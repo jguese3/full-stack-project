0882741514b94c4e439f92f2bee22866c2a56fc5
Christian Martin <cmartin60@academic.rrc.ca>
2026-01-28 21:36:42 -0600

    Update:removed hardcoded data from gamereview.tsx and put it on separate folder for now

---
+++ src/data/reviewdata.ts
@@ -0,0 +1,31 @@
+import { Review } from "../types/reviewtype";
+
+export const reviews: Review[] = [
+  {
+    id: 1,
+    username: "Christian",
+    game: "Valorant",
+    rating: 5,
+    date: "Jan 16, 2026",
+    review: "best fps game ever made",
+    likes: 1,
+  },
+  {
+    id: 2,
+    username: "Lars",
+    game: "Minecraft",
+    rating: 4,
+    date: "Jan 16, 2026",
+    review: "played this game every day for a year straight",
+    likes: 365,
+  },
+  {
+    id: 3,
+    username: "Jarone",
+    game: "God of War: Ragnarok",
+    rating: 3.5,
+    date: "Jan 16, 2026",
+    review: "played this game 67 times and died 67 times",
+    likes: 67,
+  },
+];
---
+++ src/types/reviewtype.ts
@@ -0,0 +1,9 @@
+export type Review = {
+  id: number;
+  username: string;
+  game: string;
+  rating: number;
+  date: string;
+  review: string;
+  likes: number;
+};
