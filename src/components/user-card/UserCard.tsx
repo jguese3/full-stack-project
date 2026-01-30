44861b0afbf8a9e86fe577161b079a71a48eefd9
jguese3 <jguese3@rrc.ca>
2026-01-29 23:01:18 -0600

    Feat: added user card component used to render user and their bio if clicked.

---
+++ src/components/user-card/UserCard.tsx
@@ -0,0 +1,37 @@
+import { User } from "../../assets/types/user";
+import "./user-card.css";
+
+export function UserCard(
+    {
+        user,
+        isExpanded,
+        onUsernameClick,
+        onFollowClick
+    }
+    : {
+        user: User,
+        isExpanded: boolean,
+        onUsernameClick: () => void,
+        onFollowClick: () => void
+    }
+) {
+    return (
+        <div className="user-card">
+            <div className="user-card-top">
+            {}
+            <h3 onClick={onUsernameClick}>
+                {user.userName}
+            </h3>
+            {}
+            <button onClick={onFollowClick}>
+                {user.isFollowing ? "Following" : "Unfollow"}
+            </button>
+            </div>
+            {}
+            { isExpanded
+                ? <p>{user.bio}</p>
+                : null
+            }
+        </div>
+    );
+}
