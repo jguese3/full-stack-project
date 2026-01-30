c558b52e59898b3400a43ef13ef3124231a27a5e
jguese3 <jguese3@rrc.ca>
2026-01-29 21:34:31 -0600

    adding temporary user data for my add/remove friend component

---
+++ src/assets/temp/tempUsers.tsx
@@ -0,0 +1,27 @@
+import type { User } from "../types/user";
+
+export const userData: User[] = [
+    { "id": 0, "userName": "pixelKnight", "isFollowing": false },
+    { "id": 1, "userName": "retroGamer92", "isFollowing": false },
+    { "id": 2, "userName": "noobSlayer", "isFollowing": false },
+    { "id": 3, "userName": "arcadeQueen", "isFollowing": false },
+    { "id": 4, "userName": "consoleCowboy", "isFollowing": false },
+    { "id": 5, "userName": "bossFight", "isFollowing": false },
+    { "id": 6, "userName": "savePoint", "isFollowing": false },
+    { "id": 7, "userName": "speedRunner", "isFollowing": false },
+    { "id": 8, "userName": "lootGoblin", "isFollowing": false },
+    { "id": 9, "userName": "gameOverMan", "isFollowing": false },
+    { "id": 10, "userName": "questGiver", "isFollowing": false },
+    { "id": 11, "userName": "pixelPirate", "isFollowing": false },
+    { "id": 12, "userName": "manaPotion", "isFollowing": false },
+    { "id": 13, "userName": "finalBoss", "isFollowing": false },
+    { "id": 14, "userName": "levelUp", "isFollowing": false },
+    { "id": 15, "userName": "stealthBuild", "isFollowing": false },
+    { "id": 16, "userName": "npcEnergy", "isFollowing": false },
+    { "id": 17, "userName": "hardcoreMode", "isFollowing": false },
+    { "id": 18, "userName": "coopPlayer", "isFollowing": false },
+    { "id": 19, "userName": "patchNotes", "isFollowing": false },
+    { "id": 20, "userName": "larsSy", "isFollowing": true },
+    { "id": 21, "userName": "CJM", "isFollowing": true },
+    { "id": 19, "userName": "zachM", "isFollowing": true }
+];
---
+++ src/assets/types/user.ts
@@ -0,0 +1,5 @@
+export type User = {
+    id: number,
+    userName: string,
+    isFollowing: boolean
+}
