84262401d8d0fd4c3235d8c363d01dd5133d291a
jguese3 <jguese3@rrc.ca>
2026-01-29 23:41:56 -0600

    Feat: Implemented new Following component in app.tsx and added the shared state of user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
-import { Header } from './components/layout/header/header'
-import { Footer } from './components/layout/footer/footer'
+import { useState } from 'react';
+
+import { Header } from './components/layout/header/header';
+import { Footer } from './components/layout/footer/footer';
 
 
-import { Friends } from './components/friends/Friends'
-import GameReview from './components/GameReview/GameReview'
-import { AllGames } from './components/all_games/allGames'
-import { UserGames } from './components/userList/userGameList'
+import { Friends } from './components/friends/Friends';
+import GameReview from './components/GameReview/GameReview';
+import { AllGames } from './components/all_games/allGames';
+import { UserGames } from './components/userList/userGameList';
+import Following from './components/friends/Following';
 
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
+// Mock data imports
+import type { User } from './assets/types/user';
+import { userData } from './assets/temp/tempUsers';
+
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
 
 function App() {
+  // Shared state concerning user data
+  const [users, setUsers] = useState<User[]>(userData);
   return (
     <>
       <Header />
@@ -21,7 +31,13 @@ function App() {
         <Route path="/all-games" element={<AllGames />} />
         <Route path="/UserGames" element={<UserGames />} />
         <Route path="/reviews" element={<GameReview />} />
-        <Route path="/friends" element={<Friends />} />
+        <Route path="/friends" element={
+          <>
+          <Following users={users} updateFollowing={setUsers} />
+          <Friends />
+          </>
+        } 
+        />
       </Routes>
       <Footer />
     </>
