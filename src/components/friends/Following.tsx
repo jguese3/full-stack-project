f1f49fa7e006290f173749df8cad302481aa26b7
jguese3 <jguese3@rrc.ca>
2026-03-01 21:23:40 -0600

    code cleanup

--- src/components/friends/Following.tsx
+++ src/components/friends/Following.tsx
@@ -1,61 +1,12 @@
-// import { useEffect, useState } from "react";
-// import type { User } from "../../types/user";
-// import { UserListDisplay } from "../user-list-display/UserListDisplay";
-// import { fetchUsers, followUser, unfollowUser } from "../../apis/usersRepository";
-
-// export function Following() {
-//   const [users, setUsers] = useState<User[]>([]);
-
-//   // Load users on mount
-//   useEffect(() => {
-//     setUsers(fetchUsers());
-//   }, []);
-
-//   const handleFollowToggle = async (id: number) => {
-//     const user = users.find(u => u.id === id);
-//     if (!user) return;
-
-//     if (user.isFollowing) {
-//       await unfollowUser(id);
-//     } else {
-//       await followUser(id);
-//     }
-
-//     // Refresh from repo
-//     setUsers(fetchUsers());
-//   };
-
-//   return (
-//     <section className="friend-status">
-//       <header>
-//         <h2>Following</h2>
-//       </header>
-
-//       <main>
-//         <UserListDisplay
-//           users={users.filter(user => user.isFollowing)}
-//           onFollowClick={handleFollowToggle}
-//         />
-//       </main>
-//     </section>
-//   );
-// }
-
-
 import { useUser } from "../../hooks/useUser";
 import type { User } from "../../types/user";
 import { UserListDisplay } from "../user-list-display/UserListDisplay";
-
 
 type UserListPageProps = {
     userDependencies: any[],
     userFilterFn: ((user: User) => boolean)|null
 }
 
-/** 
- * This "wrapper" component for Term Lists lets us reuse the popup and 
- * term-favourite behaviour for wrapped lists.
- */
 export function Following({userDependencies, userFilterFn}: UserListPageProps) {
 
     const { users, error, toggleFollow } = useUser(userDependencies, userFilterFn);
@@ -64,7 +15,8 @@ export function Following({userDependencies, userFilterFn}: UserListPageProps) {
         await toggleFollow(id);
     }
     return (
-        <>
+        <section className="friend-status">
+          <h2>Following</h2>
             {error ? (
                 <span className="error">Something went wrong: ({error})</span>
             ) : (
@@ -73,7 +25,7 @@ export function Following({userDependencies, userFilterFn}: UserListPageProps) {
                     onFollowClick={handleFollowClick}
                 />
             )}
-        </>
+        </section>
     );
 }
 
